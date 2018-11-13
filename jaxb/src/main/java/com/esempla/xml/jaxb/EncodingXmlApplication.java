298fdfc031ba1094f8e59570cc54f8d2cc3cb251
mrdantutunaru <danulutu@mail.ru>
2018-11-13 17:23:41 +0200

    Marshall and Unmarshall

--- jaxb/src/main/java/com/esempla/xml/jaxb/EncodingXmlApplication.java
+++ jaxb/src/main/java/com/esempla/xml/jaxb/EncodingXmlApplication.java
@@ -1,5 +1,6 @@
 package com.esempla.xml.jaxb;
 
+import com.esempla.xml.jaxb.service.MarshallAndUnMarshall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,8 @@ public class EncodingXmlApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EncodingXmlApplication.class, args);
+        MarshallAndUnMarshall cool = new MarshallAndUnMarshall();
+        cool.marshallExample();
+        cool.unmarshallExample();
     }
 }
