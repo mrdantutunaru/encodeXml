049ed4d1cc63529278557b501edc7ea8091e6dc1
mrdantutunaru <danulutu@mail.ru>
2018-11-13 14:13:27 +0200

    First Commit

---
+++ jaxb/src/main/java/com/esempla/xml/jaxb/EncodingXmlApplication.java
@@ -0,0 +1,12 @@
+package com.esempla.xml.jaxb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncodingXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncodingXmlApplication.class, args);
+    }
+}
