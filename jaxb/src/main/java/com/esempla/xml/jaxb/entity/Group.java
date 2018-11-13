72ed3bfc3a290015e68a42ee3f6f38dd416b9b47
mrdantutunaru <danulutu@mail.ru>
2018-11-13 14:24:56 +0200

    Added Person Class

--- jaxb/src/main/java/com/esempla/xml/jaxb/entity/Group.java
+++ jaxb/src/main/java/com/esempla/xml/jaxb/entity/Group.java
@@ -1,4 +1,27 @@
 package com.esempla.xml.jaxb.entity;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(propOrder = { "name", "members" }, name = "group")
+@XmlRootElement
 public class Group {
+    private String name;
+    private List<Person> members = new ArrayList<Person>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Person> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Person> members) {
+        this.members = members;
+    }
 }
